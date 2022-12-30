b1ed37f275941dd7cdf6777831479469a00c00c4
Fatih Cetinkaya <965295+devfacet@users.noreply.github.com>
2022-12-30 16:19:34 -0500

    Implement Progress.Stop

--- progress.go
+++ progress.go
@@ -6,19 +6,28 @@ package streamy
 import (
 	"errors"
 	"math"
+	"sync"
 	"time"
 )
 
+// ErrProgressStopped means that a progress stopped (i.e. by calling Stop method).
+var ErrProgressStopped = errors.New("stopped")
+
 // Progress implements the io.Writer interface for tracking bytes.
 type Progress struct {
-	bytesWritten    int64
-	totalBytes      int64
-	statsEnabled    bool
-	statsMode       ProgressStatsMode
-	statsBytes      [][]int64
-	statsBytesLimit int
-	statsFrom       int64
-	statsTo         int64
+	bytesWritten     int64
+	totalBytes       int64
+	statsEnabled     bool
+	statsMode        ProgressStatsMode
+	statsBytes       [][]int64
+	statsBytesLimit  int
+	statsFrom        int64
+	statsTo          int64
+	controlsEnabled  bool
+	controlsClosedCh <-chan struct{}
+	controlsStopCh   chan struct{}
+	rwMu             sync.RWMutex
+	stopped          bool
 }
 
 // Write implements the io.Writer interface.
@@ -26,6 +35,12 @@ func (progress *Progress) Write(p []byte) (n int, err error) {
 	// Init vars
 	n = len(p)
 	ni := int64(n)
+
+	progress.rwMu.RLock()
+	defer progress.rwMu.RUnlock()
+
+	// Update written bytes
+	progress.bytesWritten += ni
 
 	// Update stats
 	if progress.statsEnabled {
@@ -43,8 +58,17 @@ func (progress *Progress) Write(p []byte) (n int, err error) {
 		}
 	}
 
-	// Update written bytes
-	progress.bytesWritten += ni
+	// Check controls
+	if progress.controlsEnabled {
+		select {
+		case <-progress.controlsClosedCh:
+		case _, ok := <-progress.controlsStopCh:
+			if !ok {
+				progress.stopped = true
+				return 0, ErrProgressStopped
+			}
+		}
+	}
 
 	// Update stats
 	if progress.statsEnabled {
@@ -56,7 +80,7 @@ func (progress *Progress) Write(p []byte) (n int, err error) {
 }
 
 // BytesWritten returns the number of bytes written.
-func (progress Progress) BytesWritten() int64 {
+func (progress *Progress) BytesWritten() int64 {
 	return progress.bytesWritten
 }
 
@@ -66,12 +90,14 @@ func (progress *Progress) SetTotalSize(size int64, unit BinaryUnit) {
 }
 
 // TotalBytes returns the total number of bytes.
-func (progress Progress) TotalBytes() int64 {
+func (progress *Progress) TotalBytes() int64 {
 	return progress.totalBytes
 }
 
 // EnableStats enables the progress stats.
 func (progress *Progress) EnableStats(mode ProgressStatsMode) error {
+	progress.rwMu.Lock()
+	defer progress.rwMu.Unlock()
 	if !progress.statsEnabled {
 		switch mode {
 		case ProgressStatsModeSimple:
@@ -87,6 +113,8 @@ func (progress *Progress) EnableStats(mode ProgressStatsMode) error {
 
 // DisableStats disables the progress stats.
 func (progress *Progress) DisableStats() {
+	progress.rwMu.Lock()
+	defer progress.rwMu.Unlock()
 	if progress.statsEnabled {
 		progress.statsBytes = nil
 		progress.statsBytesLimit = 0
@@ -96,8 +124,35 @@ func (progress *Progress) DisableStats() {
 	progress.statsEnabled = false
 }
 
+// EnableControls enables the progress controls such as pause and stop.
+func (progress *Progress) EnableControls() error {
+	progress.rwMu.Lock()
+	defer progress.rwMu.Unlock()
+	if !progress.controlsEnabled {
+		// Ref: https://go.dev/ref/spec#Receive_operator
+		//			https://go.dev/ref/spec#Send_statements
+		//			https://go.dev/ref/spec#Close
+		ch := make(chan struct{})
+		close(ch)
+		progress.controlsClosedCh = ch
+		progress.controlsStopCh = make(chan struct{})
+	}
+	progress.controlsEnabled = true
+	return nil
+}
+
+// Stop stops the the progress writer.
+func (progress *Progress) Stop() {
+	progress.rwMu.Lock()
+	defer progress.rwMu.Unlock()
+	if !progress.controlsEnabled || progress.stopped {
+		return
+	}
+	close(progress.controlsStopCh)
+}
+
 // Stats returns the progress stats.
-func (progress Progress) Stats() ProgressStats {
+func (progress *Progress) Stats() ProgressStats {
 	if !progress.statsEnabled {
 		// Do not return anything to avoid confusion
 		return ProgressStats{}
@@ -111,6 +166,7 @@ func (progress Progress) Stats() ProgressStats {
 	}
 
 	// Calculate the percentage
+	// Note that totalBytes is given by the user and it can be 0 (see progress.SetTotalSize).
 	if progress.totalBytes > 0 {
 		if progress.totalBytes == progress.bytesWritten {
 			stats.Percentage = 100
@@ -142,7 +198,8 @@ func (progress Progress) Stats() ProgressStats {
 		}
 	}
 
-	// Calculate remaning seconds
+	// Calculate remaining seconds
+	// Note that totalBytes is given by the user and it can be 0 (see progress.SetTotalSize).
 	if progress.totalBytes > 0 {
 		if progress.totalBytes == progress.bytesWritten {
 			stats.Remaining = 0
--- progress_test.go
+++ progress_test.go
@@ -14,53 +14,23 @@ import (
 
 func TestProgress(t *testing.T) {
 	table := []struct {
-		reader              io.Reader
-		totalSize           int64
-		statsEnabled        bool
-		statsBytesWritten   int64
-		statsBytesPerSecond int64
-		statsTotalBytes     int64
-		statsRemaining      time.Duration
-		statsPercentage     int
+		reader    io.Reader
+		totalSize int64
 	}{
 		{
-			reader:              bytes.NewBufferString("foo"),
-			totalSize:           3,
-			statsEnabled:        false,
-			statsBytesWritten:   0,
-			statsBytesPerSecond: 0,
-			statsTotalBytes:     0,
-			statsRemaining:      0,
-			statsPercentage:     0,
+			reader:    bytes.NewBufferString("foo"),
+			totalSize: 3,
 		},
 		{
-			reader:              bytes.NewBufferString("bar"),
-			totalSize:           3,
-			statsEnabled:        true,
-			statsBytesWritten:   3,
-			statsBytesPerSecond: 3,
-			statsTotalBytes:     3,
-			statsRemaining:      0,
-			statsPercentage:     100,
+			reader:    bytes.NewBufferString("bar"),
+			totalSize: 3,
 		},
 		{
-			reader:              &slowReader{content: "foo bar baz"},
-			totalSize:           11,
-			statsEnabled:        true,
-			statsBytesWritten:   11,
-			statsBytesPerSecond: 6,
-			statsTotalBytes:     11,
-			statsRemaining:      0,
-			statsPercentage:     100,
+			reader: bytes.NewBufferString(""),
 		},
 	}
 	for _, v := range table {
 		progress := streamy.Progress{}
-		if v.statsEnabled {
-			if err := progress.EnableStats(streamy.ProgressStatsModeSimple); err != nil {
-				t.Errorf("got %v, want nil", err)
-			}
-		}
 		if v.totalSize > 0 {
 			progress.SetTotalSize(v.totalSize, streamy.Byte)
 		}
@@ -70,22 +40,189 @@ func TestProgress(t *testing.T) {
 		} else if written != v.totalSize {
 			t.Errorf("got %v, want %v", written, v.totalSize)
 		}
+	}
+}
+
+func TestProgressStats(t *testing.T) {
+	delay := 10 * time.Millisecond
+	table := []struct {
+		reader              io.Reader
+		totalSize           int64
+		statsBytesPerSecond int64
+		statsBytesWritten   int64
+		statsPercentage     int
+		statsRemaining      time.Duration
+		statsTotalBytes     int64
+		goEnableStats       bool
+	}{
+		{
+			reader:              bytes.NewBufferString("foo"),
+			statsBytesPerSecond: 3,
+			statsBytesWritten:   3,
+			statsPercentage:     100,
+			statsRemaining:      0,
+		},
+		{
+			reader:              bytes.NewBufferString("bar"),
+			statsBytesPerSecond: 3,
+			statsBytesWritten:   3,
+			statsPercentage:     100,
+			statsRemaining:      0,
+			statsTotalBytes:     3,
+			totalSize:           3,
+		},
+		{
+			reader:              &slowReader{content: "foo bar baz", delay: 200 * time.Millisecond},
+			totalSize:           11,
+			statsBytesPerSecond: 6,
+			statsBytesWritten:   11,
+			statsPercentage:     100,
+			statsRemaining:      0,
+			statsTotalBytes:     11,
+		},
+		{
+			reader:              &slowReader{content: "foo bar baz", delay: 10 * time.Millisecond},
+			totalSize:           11,
+			statsBytesPerSecond: 11,
+			statsBytesWritten:   11,
+			statsPercentage:     100,
+			statsRemaining:      0,
+			statsTotalBytes:     11,
+			goEnableStats:       true,
+		},
+	}
+	for _, v := range table {
+		progress := streamy.Progress{}
+		if v.totalSize > 0 {
+			progress.SetTotalSize(v.totalSize, streamy.Byte)
+		}
+		if v.goEnableStats {
+			go func() {
+				time.Sleep(delay * 2)
+				if err := progress.EnableStats(streamy.ProgressStatsModeSimple); err != nil {
+					t.Errorf("got %v, want nil", err)
+				}
+			}()
+		} else {
+			if err := progress.EnableStats(streamy.ProgressStatsModeSimple); err != nil {
+				t.Errorf("got %v, want nil", err)
+			}
+		}
+		written, err := io.Copy(io.Discard, io.TeeReader(v.reader, &progress))
+		if err != nil {
+			t.Errorf("got %v, want nil", err)
+		}
+		if v.totalSize > 0 && written != v.totalSize {
+			t.Errorf("got %v, want %v", written, v.totalSize)
+		}
 		stats := progress.Stats()
 		if stats.BytesWritten != v.statsBytesWritten {
 			t.Errorf("got %v, want %v", stats.BytesWritten, v.statsBytesWritten)
 		} else if stats.BytesPerSecond != v.statsBytesPerSecond {
 			t.Errorf("got %v, want %v", stats.BytesPerSecond, v.statsBytesPerSecond)
-		} else if stats.TotalBytes != v.statsTotalBytes {
-			t.Errorf("got %v, want %v", stats.TotalBytes, v.statsTotalBytes)
 		} else if stats.Remaining != v.statsRemaining {
 			t.Errorf("got %v, want %v", stats.Remaining, v.statsRemaining)
-		} else if stats.Percentage != v.statsPercentage {
-			t.Errorf("got %v, want %v", stats.Percentage, v.statsPercentage)
+		}
+		if v.totalSize > 0 {
+			if v.totalSize > 0 && stats.TotalBytes != v.statsTotalBytes {
+				t.Errorf("got %v, want %v", stats.TotalBytes, v.statsTotalBytes)
+			} else if stats.Percentage != v.statsPercentage {
+				t.Errorf("got %v, want %v", stats.Percentage, v.statsPercentage)
+			}
 		}
 	}
 }
 
-func BenchmarkProgressStatsDisabled(b *testing.B) {
+func TestProgressControls(t *testing.T) {
+	delay := 10 * time.Millisecond
+	table := []struct {
+		reader           io.Reader
+		totalSize        int64
+		goEnableControls bool
+		stopCall         bool
+		dupStopCall      bool
+		enableStats      bool
+	}{
+		{
+			reader:    &slowReader{content: "foo bar baz", delay: delay},
+			totalSize: 11,
+		},
+		{
+			reader:      &slowReader{content: "foo bar baz", delay: delay},
+			totalSize:   11,
+			enableStats: true,
+		},
+		{
+			reader:           &slowReader{content: "foo bar baz", delay: delay},
+			totalSize:        11,
+			goEnableControls: true,
+		},
+		{
+			reader:           &slowReader{content: "foo bar baz", delay: delay},
+			totalSize:        11,
+			goEnableControls: true,
+			enableStats:      true,
+		},
+		{
+			reader:      &slowReader{content: "foo bar baz", delay: delay},
+			totalSize:   11,
+			dupStopCall: true,
+		},
+		{
+			reader:           &slowReader{content: "foo bar baz", delay: delay},
+			totalSize:        11,
+			goEnableControls: true,
+			dupStopCall:      true,
+		},
+	}
+	for _, v := range table {
+		progress := streamy.Progress{}
+		if v.enableStats {
+			if err := progress.EnableStats(streamy.ProgressStatsModeSimple); err != nil {
+				t.Errorf("got %v, want nil", err)
+			}
+		}
+		if v.goEnableControls {
+			go func() {
+				time.Sleep(delay * 2)
+				if err := progress.EnableControls(); err != nil {
+					t.Errorf("got %v, want nil", err)
+				}
+			}()
+		} else {
+			if err := progress.EnableControls(); err != nil {
+				t.Errorf("got %v, want nil", err)
+			}
+		}
+		if v.stopCall {
+			go func() {
+				time.Sleep(delay * 4)
+				progress.Stop()
+			}()
+			if v.dupStopCall {
+				go func() {
+					time.Sleep(delay * 4)
+					progress.Stop()
+				}()
+			}
+		}
+		written, err := io.Copy(io.Discard, io.TeeReader(v.reader, &progress))
+		if err != nil {
+			t.Errorf("got %v, want nil", err)
+		}
+		if v.stopCall {
+			if written <= 0 || written >= v.totalSize {
+				t.Errorf("got %v, want >0 <%v", written, v.totalSize)
+			}
+		} else {
+			if written != v.totalSize {
+				t.Errorf("got %v, want %v", written, v.totalSize)
+			}
+		}
+	}
+}
+
+func BenchmarkProgress(b *testing.B) {
 	progress := streamy.Progress{}
 	for i := 0; i < b.N; i++ {
 		io.TeeReader(bytes.NewBufferString("foo"), &progress)
@@ -95,6 +232,16 @@ func BenchmarkProgressStatsDisabled(b *testing.B) {
 func BenchmarkProgressStatsEnabled(b *testing.B) {
 	progress := streamy.Progress{}
 	if err := progress.EnableStats(streamy.ProgressStatsModeSimple); err != nil {
+		b.Errorf("got %v, want nil", err)
+	}
+	for i := 0; i < b.N; i++ {
+		io.TeeReader(bytes.NewBufferString("foo"), &progress)
+	}
+}
+
+func BenchmarkControlsEnabled(b *testing.B) {
+	progress := streamy.Progress{}
+	if err := progress.EnableControls(); err != nil {
 		b.Errorf("got %v, want nil", err)
 	}
 	for i := 0; i < b.N; i++ {
@@ -116,12 +263,13 @@ func BenchmarkProgressStats(b *testing.B) {
 // slowReader implements a slow reader.
 type slowReader struct {
 	content string
+	delay   time.Duration
 	written int64
 }
 
 // Read implements the io.Reader interface.
 func (sr *slowReader) Read(p []byte) (n int, err error) {
-	defer time.Sleep(200 * time.Millisecond) // slow it down
+	defer time.Sleep(sr.delay)
 
 	p[0] = []byte(sr.content[sr.written : sr.written+1])[0]
 	sr.written++
